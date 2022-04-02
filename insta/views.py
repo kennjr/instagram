3f3877059d012e3a1d06f6f13da6296e3aa3f51a
kennjr <ken.juniour@student.moringaschool.com>
2022-04-03 02:23:22 +0300

    Added code for getting the search string from the input filed in the index html to the home view fun

--- insta/views.py
+++ insta/views.py
@@ -4,6 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     title = 'Instagram - Home'
-    context = {'title': title}
+    search_req = request.GET.get('search_str') if request.GET.get('search_str') is not None else ''
+
+    if search_req != '':
+        context = {'title': title, 'mode': "browse"}
+    else:
+        context = {'title': title, 'mode': "search"}
     return render(request, 'insta/index.html', context)
 
