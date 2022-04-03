566ee3cce6340331776ee5d4c18c88e838695f8f
kennjr <ken.juniour@student.moringaschool.com>
2022-04-03 06:18:24 +0300

    Created the view fun for the search page

--- insta/views.py
+++ insta/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def home(request):
     title = 'Instagram - Home'
-    search_req = request.GET.get('search_str') if request.GET.get('search_str') is not None else ''
 
-    if search_req != '':
-        context = {'title': title, 'mode': "browse"}
-    else:
-        context = {'title': title, 'mode': "search"}
+    context = {'title': title}
     return render(request, 'insta/index.html', context)
 
+
+def search(request):
+    title = 'Instagram - Home'
+    search_req = request.GET.get('q') if request.GET.get('q') is not None else ''
+
+    if search_req == '':
+        is_req_empty = True
+    else:
+        # todo Make a search request for posts with a location/user/caption that matches the str
+        is_req_empty = False
+    context = {'title': title}
+    return render(request, 'insta/search.html', context)
