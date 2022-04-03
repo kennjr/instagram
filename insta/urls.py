4bb40271b4feeb7701b632f14a8a10b01119e4bc
kennjr <ken.juniour@student.moringaschool.com>
2022-04-03 06:18:48 +0300

    Added the path for the search page to the urlpatterns list

--- insta/urls.py
+++ insta/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from insta import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
 ]
