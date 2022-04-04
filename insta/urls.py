dd0d1b63b1fb31fa7efb0517e01140ca4fcae248
kennjr <ken.juniour@student.moringaschool.com>
2022-04-04 12:00:37 +0300

    Updated the urls file, adding code that'll allow me to process and show images

--- insta/urls.py
+++ insta/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from insta import views
 
 urlpatterns = [
+    path('login/', views.login_page, name='login_page'),
+    path('register/', views.register_page, name='register_page'),
     path('', views.home, name='home'),
     path('search/', views.search, name='search'),
     path('users/<int:uid>/', views.profile, name='profile'),
