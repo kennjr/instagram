59d2769923d822ce77404a2970f33e0021d734b8
kennjr <ken.juniour@student.moringaschool.com>
2022-04-04 12:19:25 +0300

    Updated the admin class, adding the code for modifying a profile and user

--- insta/admin.py
+++ insta/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+# This class determines how the admin dashboard will look
+from insta.models import User, Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    list_display = ('username', 'email', 'created')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('username', "created")
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    # The line below will allow us to display the meetup obj items in a list with the specified columns
+    # the columnnames in the models are what we've used in the strs
+    list_display = ('user', 'updated', 'website')
+    # The line below will allow us to have filter opts for our list of entries that appear in the admin dash
+    list_filter = ('user', "created")
+    # The line below will pre-populate the slug field based on the title we enter,
+    #  the key is the param that we'd like to pre-populate and the tuple has the entries that'll be concat'ed to create the key
+    # prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
+
